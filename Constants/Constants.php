097ed1da123d03c3e35c63d621d9fc944a17d560
Ahmed Mera <ahmedmera81136@gmail.com>
2021-04-02 23:04:42 +0200

    [master] - (ConfigurationDB, index.php, main.css) - add css and structure of page
    Signed-off-by: Ahmed Mera <ahmedmera81136@gmail.com>

---
+++ Constants/Constants.php
@@ -0,0 +1,34 @@
+<?php
+/*
+ *
+ *  *  Copyright (c) 2021
+ *  *  Version: AM FREE 1.0.0
+ *  *
+ *  *  Copyright: Ahmed Mera
+ *  *  https://mera.ddns.net
+ *  *
+ *  *  Contact: meraahmedibrahim@itis-molinari.eu
+ *
+ */
+
+final class Constants {
+
+    protected string $ERROR_MSG = "Qualcosa è andato storto, riprova ancora oppure comunica l'amministratore col codice di error  ";
+
+    /**
+     * helper function to generate msg error
+     * @param string $codErro
+     * @return string
+     * @author Ahmed Mera
+     */
+    public function getErrorMSG(string $codErro): string
+    {
+        return  $this->ERROR_MSG . "<b> {$codErro} </b>" ;
+    }
+
+
+
+
+
+
+}
--- index.php
+++ index.php
@@ -12,7 +12,6 @@
  */
 
 
-
 ?>
 
 
@@ -32,8 +31,73 @@
 </head>
 <body>
 
+<div class="container-fluid">
+    <h1 class="center-align title "> DATABASE </h1>
+    <div class="row">
+        <!--   teoria     -->
+        <div class="col m6 s12 teoria center-align ">
+            <div class="content z-depth-3">
+                <h2>Teoria</h2>
+
+                <!--        select argument        -->
+                <div class="input-field col s12">
+                    <select>
+                        <option value="" disabled selected>Scegli il tuo argomento:</option>
+                        <option value="1">Option 1</option>
+                        <option value="2">Option 2</option>
+                        <option value="3">Option 3</option>
+                    </select>
+                </div>
+
+                <p class="flow-text">I am Flow Text</p>
+            </div>
+        </div>
+
+        <!--   pratica  -->
+        <div class="col m6 s12 pratica center-align ">
+
+            <div class="content z-depth-3">
+                <h2>Pratica</h2>
+
+                <!--      query space          -->
+                <div class="row ">
+                    <form class="col s12">
+                        <div class="row">
+                            <div class="input-field col s11 query">
+                                <i class="fas fa-database material-icons prefix"></i>
+<!--                                <i class="material-icons  prefix">mode_edit</i>-->
+                                <textarea id="icon_prefix2" class="materialize-textarea"></textarea>
+                                <label for="icon_prefix2">Scrivi la tua query</label>
+                            </div>
+
+                            <div class="col offset-s9">
+                                <button class="btn waves-effect waves-light" disabled type="submit" name="action">Submit
+                                    <i class="material-icons right">send</i>
+                                </button>
+                            </div>
+
+                        </div>
+                    </form>
+                </div>
+
+            </div>
+
+        </div>
+    </div>
+    <div class="row"></div>
+</div>
+<!--fontawesome -->
+<script src="https://kit.fontawesome.com/650809f241.js" crossorigin="anonymous"></script>
+<!-- jquery -->
+<script src="https://code.jquery.com/jquery-2.1.1.min.js" ></script>
 <!-- Compiled and minified JavaScript -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+
+<script>
+    $(document).ready(function(){
+        $('select').formSelect();
+    });
+</script>
 <!-- my js   -->
 <script src="./js/main.js"></script>
 </body>
